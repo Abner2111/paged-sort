f58a35b37f29bcd4f212eb50f15a93efe5dd73bc
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2021-08-29 14:51:20 -0600

    Finished paging functionality

--- main.cpp
+++ main.cpp
@@ -2,45 +2,105 @@
  * @author Abner Arroyo
  * @date 8/28/2021
  * @brief program that sorts a large file with integers with just 6 pages of 1KB of memory each
- *
  */
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <cstring>
 using namespace std;
 
 class PagedArray{
 public:
-
     int size;
-    int virtualArray[256*3][2];
     ifstream infile;
     ofstream outfile;
+    string copyfilename;
+    string outputfilename;
 
     PagedArray(string inputfilename, string outputfilename){
-        infile.open(inputfilename);
-        outfile.open(outputfilename);
+        this->outputfilename = outputfilename;
+        this->outfile.open(outputfilename);
+        this->infile.open(inputfilename);
+        this->copyfilename = "newoutput.txt";
         string line;
         this->size = 0;
-        if(infile.is_open() && outfile.is_open()){
-            while(!infile.eof()){
-                getline(infile, line);
-                outfile << line << "\n";
+        if(this->infile.is_open() && this->outfile.is_open()){
+            while(!this->infile.eof()){
+                getline(this->infile, line);
+                this->outfile << line << endl;
                 this->size++;
             }
-            cout <<"File has " << size << "entries" << endl;
             cout << "Data copied to new location..."<< endl;
+            cout << "The file has " << this->size << " entries" << endl;
 
         } else {
             printf("Cannot read file");
         }
-        infile.close();
+        this->infile.close();
+        this->outfile.close();
 
+
+    }
+    /***
+     *
+     * @param i
+     * @return the value of the element in the requestedd index from the paged array
+     * @brief it overloads the subscripting operator
+     */
+    int& operator[](int i){
+        int pi = 0;
+        for (pi;pi<this->size;pi++){
+            if(this->virtualArray[pi][0]==i){
+                return this->virtualArray[pi][1];
+            } else {
+                cout << "page fault" << endl;
+                //pageFault
+                return this->replace(i);
+            }
+        }
     }
 
 private:
-    int calculatefilesize(string filename){
+    int virtualArray[256*3][2];
+    int nextflush = 0;
 
+    /***
+     *
+     * @param i
+     * @return the int& from the array, tranlated from the virtual memory after being inserted in it
+     */
+    int& replace(int i){
+        int pos = 0;
+        infile.open(this->outputfilename);
+        outfile.open(copyfilename);
+        string line;
+        int curr;
+
+        if(this->infile.is_open() && this->outfile.is_open()){
+            while(!this->infile.eof()){
+                getline(this->infile, line);
+                if(pos == this->nextflush){
+                    this->outfile << this->virtualArray[this->nextflush][1];
+                    getline(this->infile, line);
+                    pos++;
+                } else if(pos==i){
+                    this->virtualArray[this->nextflush][0]=i;
+                    this->virtualArray[this->nextflush][1]=stoi(line);
+                }
+                this->outfile << line << "\n";
+            }
+            curr = this->nextflush;
+            if (this->nextflush < this->size-1){
+                this->nextflush++;
+            } else {
+                this->nextflush = 0;
+            }
+            remove(this->outputfilename.c_str());
+            rename(this->copyfilename.c_str(), this->outputfilename.c_str());
+            return (int &) virtualArray[curr][1];
+        }
     }
+
 };
 
 
@@ -67,5 +127,6 @@ int sort(int array[], int size){
 }
 
 int main() {
-    cout << (sizeof(int)*2*3*256)/1024<< endl;
+    PagedArray array("randomnumbers.txt", "newfile.txt");
+    sort(&array[0], array.size);
 }
